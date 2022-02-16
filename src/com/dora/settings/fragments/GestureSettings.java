53a5563b9b645ac39edf72fe6453f1b2d65fbf81
dopaemon <polarisdp@gmail.com>
2022-02-16 04:26:20 +0000

    DoraemonOS: corvert DORA_SETTINGS
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- src/com/dora/settings/fragments/GestureSettings.java
+++ src/com/dora/settings/fragments/GestureSettings.java
@@ -18,7 +18,7 @@ public class GestureSettings extends SettingsPreferenceFragment {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.DORA;
+        return MetricsProto.MetricsEvent.DORA_SETTINGS;
     }
 
 }
